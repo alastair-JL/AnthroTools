a0069ed615a44029aa1ce5a99f3e2c4c987d2656
Babblefish <alastair_JL@xtra.co.nz>
2015-10-28 10:51:18 -0700

    Anthropac compatibility update

--- R/LoadFromAnthropac.R
+++ R/LoadFromAnthropac.R
@@ -5,36 +5,32 @@
 #' @usage LoadFromAnthropac(filename)
 #' @param filename This is the name of the file containing your anthropac data.
 #' @keywords FreeList
-#' @return A three column table containing the same data, stored in a format compatible with theAnthroTools package.
+#' @return A three column table containing the same data, stored in a format compatible with the AnthroTools package.
 #' @author Alastair Jamieson Lane. <aja107@@math.ubc.ca>
 #' @author Benjamin Grant Purzycki. <bgpurzycki@@alumni.ubc.ca>
 #' @export
 #' 
 LoadFromAnthropac <-
 function(filename){      
-  AnthroPac <- read.table(filename, quote="\"", blank.lines.skip=FALSE)
-  Tabulated<- data.frame(subj=levels(AnthroPac[,]),ORDER=1,CODE=levels(AnthroPac[,]))
-  Tabulated<-Tabulated[1,]
-  Mode<- 1
+  AnthroPac <- read.csv(filename, quote="\"",comment.char="%",header=F)
+  Tabulated<- data.frame(subj=as.character(levels(AnthroPac[,])),ORDER=1,CODE=as.character(levels(AnthroPac[,])),stringsAsFactors=FALSE)
+  Tabulated<-Tabulated[1,]  
   Order<-1
   CurrentSubj<-NULL  
   for(iii in 1:nrow(AnthroPac) ){
-    if(Mode==1){
-      Tabulated[1,1]<-AnthroPac[iii,1]
-      Tabulated[1,2]<-1
-      Mode=2
-    }else{
-      if(AnthroPac[iii,1]==""){
-        Mode=1
-      }else{
-        Tabulated[1,3]<-AnthroPac[iii,1]
+    if(substring(AnthroPac[iii,1] , 1, 1)=="#"){
+      Tabulated[1,1]<- gsub("^\\s+|\\s+$", "",substring(AnthroPac[iii,1] , 2) ) ##Drop the initial #, then trim out leading and trailing spaces.
+      Tabulated[1,2]<-1      
+    }else{    
+        Tabulated[1,3]<-as.character(AnthroPac[iii,1])
         Tabulated[nrow(Tabulated)+1,]<-Tabulated[1,]
-        Tabulated[1,2]<-Tabulated[1,2]+1
-      }
+        Tabulated[1,2]<-Tabulated[1,2]+1      
     }    
-    
+     
   }
   
-  Tabulated=Tabulated[-1,]    
+  Tabulated<-Tabulated[-1,]    
+  Tabulated<-data.frame(subj=Tabulated[,"subj"],ORDER=Tabulated[,"ORDER"],CODE=Tabulated[,"CODE"])
+  #This line is to turn all the strings back into factors for reasonable handling later on.
   return(Tabulated)
 }
