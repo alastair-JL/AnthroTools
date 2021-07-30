2ec826c54c6eed21fe5c9acb3c56cc243ef1b7c3
Benjamin Grant Purzycki <bgpurzycki@users.noreply.github.com>
2021-07-30 20:32:42 +0200

    Update GenerateFakeFreelistData.R

--- R/GenerateFakeFreelistData.R
+++ R/GenerateFakeFreelistData.R
@@ -1,6 +1,6 @@
 #' GenerateFakeFreeListData
 #'
-#' This function generates fake free-list data- both to act as a template, and incase anyone needs to run some sort of statistical analysis. 
+#' This function generates fake free-list data- both to act as a template, and in case anyone needs to run some sort of statistical analysis. 
 #' 
 #' @usage GenerateFakeFreeListData()
 #' @keywords FreeList
@@ -11,7 +11,7 @@
 #'  default settings for the other FreeList related functions in this
 #'   package. Subj specifies the subject number, CODE lists the subject's responses to a free-listing question (in this case "List whatever fruit you can think of") and Order specifies what order each subject listed their items in. The data created is entirely fictitious, and no attempt is made to prevent the imaginary subjects from listing the same fruit multiple times.
 #' @author Alastair Jamieson Lane. <aja107@@math.ubc.ca>
-#' @author Benjamin Grant Purzycki. <bgpurzycki@@alumni.ubc.ca>
+#' @author Benjamin Grant Purzycki. <bgpurzycki@@cas.au.dk>
 #' @examples
 #' fakeData<-GenerateFakeFreeListData()
 #' View(fakeData)
@@ -24,7 +24,7 @@
 #' fullData<-rbind(mainlandData,islandData,moonData) ##This method gives data with distinct groupings.
 #' 
 #' @note
-#' Depending on what is needed oruseful, we may implement a version of this function that can take some sort of statistical input (average length of peoples lists or such) and creates fake data with a particular shape. 
+#' Depending on what is needed or useful, we may implement a version of this function that can take some sort of statistical input (average length of peoples lists or such) and creates fake data with a particular shape. 
 #' This might be most easily implemented by having it use an existing data set and then mashing it up in some way so as to create a fake data set- allowing the user to compare their data to the usual in some way.
 #' Doing this would require some amount of research, and thus I would only really want to do so if there were enough interest in it.
 
