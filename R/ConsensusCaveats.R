b3f98e3caf00d34c87fa768864a23197e548561a
Benjamin Grant Purzycki <bgpurzycki@users.noreply.github.com>
2023-10-06 11:38:26 +0200

    email update

--- R/ConsensusCaveats.R
+++ R/ConsensusCaveats.R
@@ -40,7 +40,7 @@
 #'  \item Romney, A. K., Weller, S. C., & Batchelder, W. H. (1986). Culture as Consensus: A Theory of Culture and Informant Accuracy. American Anthropologist, 88(2), 313-338.  
 #'  }
 #' @author Alastair Jamieson Lane. <aja107@@math.ubc.ca>
-#' @author Benjamin Grant Purzycki. <bgpurzycki@@alumni.ubc.ca>
+#' @author Benjamin Grant Purzycki. <bgpurzycki@@cas.au.dk>
 #' 
 #' @examples
 #' x<-runif(5,0,1) ##some artificial competence levels.
