6dd1335b5f0dd01acc0e5a8f5d00b919c971e776
Benjamin Grant Purzycki <bgpurzycki@users.noreply.github.com>
2024-08-20 13:14:01 +0200

    Update triad.gen.R

--- R/triad.gen.R
+++ R/triad.gen.R
@@ -1,22 +1,22 @@
-#' Triadgen
+#' TriadGen
 #' Create triad test instrument
 #' @alias TriadGen
-#' @usage triad.gen(items)
+#' @usage TriadGen(items)
 #' @examples
 #' @param items This is a list of the items you'll use in your instrument.
 #' @return The output of this function is a randomize ready-to-use triad test.
 #' @author Benjamin Grant Purzycki. <bgpurzycki@@cas.au.dk>
 #' RVS <- c("Ben", "Uffe", "Jesper", "Lene", "Jorn", "Marianne Q.-F.", "Thomas", "Martin", "Mark") # your items
-#' triadmat <- triad.gen(RVS)
+#' triadmat <- TriadGen(RVS)
 #' View(triadmat)
 #' @export
 #' @examples
 #' RVS <- c("Ben", "Uffe", "Jesper", "Lene", "Jorn", "Marianne Q.-F.", "Thomas", "Martin", "Mark") # your items
-#' triadmat <- triad.gen(RVS)
+#' triadmat <- TriadGen(RVS)
 #' View(triadmat)
 #' 
 
-triad.gen <- function(items){
+TriadGen <- function(items){
   triadlist <- data.frame(t(combn(items, 3))) # all possible triad combs
   triadlist$NUM <- seq(1, nrow(triadlist), 1) # assign triad a number
   triadlist <- triadlist[, c(4, 1, 2, 3)] # rearrange variable order
